937d229599c6b4c258f5d83a705962f3a1626c9d
Dennis <dennis.diekmann@uni-oldenburg.de>
2022-03-14 09:29:07 +0100

    Move config

---
+++ Editor/Config/TurtleToolsConfig.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+namespace Config
+{
+    [CreateAssetMenu(fileName = "TurtleToolsConfig", menuName = "TurtleTools/Config", order = 0)]
+    public class TurtleToolsConfig : ScriptableObject
+    {
+        [Header("Directory Structure")] 
+        [SerializeField]
+        private TextAsset directoryStructureFile;
+
+        public string[] DirStructure { get; private set; }
+
+        private void OnValidate()
+        {
+            DirStructure = directoryStructureFile
+                ? directoryStructureFile.text.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries)
+                : null;
+        }
+    }
+}
